251abc3db45ac9fb80306621fe633bc9d68d3c6b
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-07-20 23:08:11 +0300

    added user aythentication to folder

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
 ]
 
 from django.urls import path
-from .views import RegisterView
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
