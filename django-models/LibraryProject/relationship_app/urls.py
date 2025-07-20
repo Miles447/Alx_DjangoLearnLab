5865e193a4b15e6bb7a55d3fdb97b3a17f14b7c1
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-07-20 23:50:14 +0300

    added role based acces control

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,10 +16,12 @@ urlpatterns = [
     path('register/', views.registerView.as_view(), name='register'),
 ]
 from django.urls import path
-from . import views
+from .admin_view import admin_view
+from .librarian_view import librarian_view
+from .member_view import member_view
 
 urlpatterns = [
-    path('admin-role/', views.admin_view, name='admin_view'),
-    path('librarian-role/', views.librarian_view, name='librarian_view'),
-    path('member-role/', views.member_view, name='member_view'),
+    path('admin-role/', admin_view, name='admin_view'),
+    path('librarian-role/', librarian_view, name='librarian_view'),
+    path('member-role/', member_view, name='member_view'),
 ]
