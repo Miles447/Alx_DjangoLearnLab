f30722207a129c882bd1e4cf4d76b8cc52beb639
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-07-27 22:36:08 +0300

    this is my second commit

--- advanced_features_and_security/LibraryProject/access_control/models.py
+++ advanced_features_and_security/LibraryProject/access_control/models.py
@@ -1,18 +1,14 @@
 from django.db import models
 
-
-class Document(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    # other fields
 
     class Meta:
         permissions = [
-            ("can_view", "Can view document"),
-            ("can_create", "Can create document"),
-            ("can_edit", "Can edit document"),
-            ("can_delete", "Can delete document"),
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
         ]
-
-    def __str__(self):
-        return self.title
