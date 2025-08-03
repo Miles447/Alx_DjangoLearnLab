aa7faf8ca033de014b854470a1758b866316f269
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-08-03 14:09:22 +0300

    undertaked CRUD operations in postman

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book')
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list')
+    #Route for the BookList view
+    path('books/', BookList.as_view(), name='book-list'),
+    
+    #include the router URLs for BookViewSet (all CRUD operations)
+    path('', include(router.urls)),
 ]
