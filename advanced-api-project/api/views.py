8651fcd6cca1b8e8d516e9fb4c5d487d9e96b41b
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-08-10 21:32:58 +0300

    implemented generic views and mixins

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from rest_framework import generics, permissions
 from .serializers import BookSerializer 
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser
 # Create your views here.
 #retrieve a single book
 
