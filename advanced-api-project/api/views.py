75b439c25a3291a1629098c6aca1b1fb39ed01b9
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-08-10 21:21:58 +0300

    implemented generic views and mixins

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Book
+from rest_framework import generics, permissions
+from .serializers import BookSerializer 
 
 # Create your views here.
+#retrieve a single book
+class BookDetailView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny] #allows anyone to view the book details
+
+#Create new book
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny] #allows anyone to create a new book
+
+#updte existing book
+class BookUpdateView(generics.UpdateAPIView):    
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny] #allows anyone to update an existing book
+
+#delete existing book    
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny] #allows anyone to delete an existing book
