61f6ccfa9c8a3c1f76c883cac2ba77ac2faeeb1d
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-08-10 22:20:10 +0300

    used filtering and ordering in my views file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics, permissions, filters
 from .serializers import BookSerializer 
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 # Create your views here.
 #retrieve a single book
 
@@ -25,7 +26,7 @@ class BookListView(generics.ListAPIView):
     #ordering_fields
     ordering_fields = ['title', 'publication_year']
     ordering = ['title'] #default order
-    
+
 class BookDetailView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
